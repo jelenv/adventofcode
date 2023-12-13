4e61ae0fe6286e42c8328bcb3569ea0bf8ae5b48
trits <trits@users.noreply.github.com>
2023-12-13 22:33:32 +0100

    add part of day4

--- 2023/day04/solve.go
+++ 2023/day04/solve.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc23/utils"
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -12,6 +13,17 @@ func main() {
 
 func part1(inputFile string) {
 	data := utils.ReadFileData(inputFile)
+
+	lines := strings.Split(data, "\n")
+	for _, line := range lines {
+		// split line by : this gets us card and numbers
+		parts := strings.Split(line, ":")
+		card := parts[0]
+		numbers := parts[1]
+		parts = strings.Split(numbers, "|")
+
+	}
+
 	fmt.Printf("Data part1: %s\n", data)
 }
 
