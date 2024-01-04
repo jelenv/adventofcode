eba4500948556b7ecfc78d2e38d69fa77ccb8be9
trits <trits@users.noreply.github.com>
2024-01-04 22:10:26 +0100

    04/23: add small progress

--- 2023/day04/solve.go
+++ 2023/day04/solve.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	part1("test.txt")
-	part2("test.txt")
+	// part2("test.txt")
 }
 
 func part1(inputFile string) {
@@ -19,15 +19,23 @@ func part1(inputFile string) {
 		// split line by : this gets us card and numbers
 		parts := strings.Split(line, ":")
 		card := parts[0]
+		println(card)
 		numbers := parts[1]
 		parts = strings.Split(numbers, "|")
+		winningNumbers := strings.Split(strings.TrimSpace(parts[0]), " ")
+		myNumbers := strings.Split(strings.TrimSpace(parts[1]), " ")
+
+		fmt.Printf("Winning nums: %+q\n", winningNumbers)
+		fmt.Printf("My nums: %+q\n", myNumbers)
+
+		break
 
 	}
 
-	fmt.Printf("Data part1: %s\n", data)
+	// fmt.Printf("Data part1: %s\n", data)
 }
 
 func part2(inputFile string) {
-	data := utils.ReadFileData(inputFile)
-	fmt.Printf("Data part1: %s\n", data)
+	// data := utils.ReadFileData(inputFile)
+	// fmt.Printf("Data part1: %s\n", data)
 }
